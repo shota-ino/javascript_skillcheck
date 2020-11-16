33f9bb38d43cf0949976891619b895e053be269b
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-16 12:50:15 +0900

    マウスオーバー＆アウトしたらイベントが発火を実装

--- app/javascript/hover.js
+++ app/javascript/hover.js
@@ -1,5 +1,19 @@
 function hover() {
-  console.log("マウスオーバーによる色彩変更機能を実装");
+  // console.log("マウスオーバーによる色彩変更機能を実装");（問題1で試しに記述）
+
+  // 発火したい要素が複数のため、事前にすべての要素をquerySelectorAllを用いて取得
+  const articles = document.querySelectorAll(".article");
+  // 取得した複数の要素にforEachを用いて、すべての要素においてイベント発火ができるように
+  articles.forEach((article) => {
+    // マウスオーバーしたときのイベントハンドラー
+    article.addEventListener('mouseover', () => {
+      console.log("マウスオーバー");
+    });
+    // マウスアウトしたときのイベントハンドラー、mouseoutを用いて設定
+    article.addEventListener('mouseout', () => {
+      console.log("マウスアウト");
+    });
+  });
 }
 
 window.addEventListener('load', hover);
